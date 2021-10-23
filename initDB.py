09bee4518a802418b2086ddf8b98a9c36e175b85
Ashton Hammond <hammosam@byu.edu>
2021-10-23 15:24:20 -0600

    Add dummy data

--- initDB.py
+++ initDB.py
@@ -67,11 +67,11 @@ cursor.execute(
 cursor.execute(
   """
   CREATE TABLE IF NOT EXISTS Params (
-  param_id      INT           NOT NULL,
-  name          VARCHAR(255)  NOT NULL,
-  game_id       INT           NOT NULL,
-  description   VARCHAR(255)  NOT NULL,
-  value         INT           NOT NULL,
+  param_id      INT AUTO_INCREMENT  NOT NULL,
+  name          VARCHAR(255)        NOT NULL,
+  game_id       INT                 NOT NULL,
+  description   VARCHAR(255)        NOT NULL,
+  value         INT                 NOT NULL,
   PRIMARY KEY (param_id),
   FOREIGN KEY (game_id) REFERENCES Games(game_id)
   )
@@ -89,11 +89,69 @@ cursor.execute(
   )
   """)
 
-# TODO: @Ashton fill with dummy data
+# Fill with dummy data
 print("Filling with dummy data...")
+# Users
 cursor.execute("INSERT INTO users VALUES ('brody', 'password', 'brody@rasmus.sen')")
-cursor.execute("INSERT INTO games (name, publisher, description, username) VALUES ('Catan', 'pub', 'get sheep', 'brody')")
-# cursor.execute("INSERT INTO ... ")
+cursor.execute("INSERT INTO uesrs VALUES ('ashton', 'password1', 'ashto@hammo.com')")
+cursor.execute("INSERT INTO users VALUES ('braydon', 'password2', 'braydon@hunt.com')")
+cursor.execute("INSERT INTO uesrs VALUES ('jared', 'password3', 'jared@jacob.com')")
+cursor.execute("INSERT INTO uesrs VALUES ('nate', 'password4', 'nate@fasta.bend')")
+# Games
+cursor.execute("INSERT INTO games (name, publisher, description, username) VALUES ('Catan', 'Kosmos', 'Settle a new land!', 'brody')")
+cursor.execute("INSERT INTO games (name, publisher, description, username) VALUES ('Ticket to Ride', 'Days of Wonder', 'Build choo-choo trains!', 'ashton')")
+cursor.execute("INSERT INTO games (name, publisher, description, username) VALUES ('Carcassonne', 'Rio Grande Games', 'Shape the medieval landscape of France.', 'braydon')")
+cursor.execute("INSERT INTO games (name, publisher, description, username) VALUES ('Terraform Mars', 'FryxGames', 'Make Mars habitable!', 'jared')")
+cursor.execute("INSERT INTO games (name, publisher, description, username) VALUES ('7 Wonders', 'Repos Production', 'Earn points by building stuff with cards.', 'nate')")
+# Scores
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (0, 11, 'brody')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (0, 10, 'ashton')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (1, 190, 'jared')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (1, 210, 'nate')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (2, 303, 'nate')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (2, 295, 'brody')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (3, 132, 'nate')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (3, 112, 'brayden')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (4, 79, 'ashton')")
+cursor.execute("INSERT INTO scores (game_id, value, username) VALUES (4, 76, 'jared')")
+# Params
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 0, 'number of players', 4)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 0, 'number of rounds to finish', 20)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 0, 'number of players', 5)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 0, 'number of rounds to finish', 18)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 1, 'number of players', 4)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 1, 'number of rounds to finish', 20)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 1, 'number of players', 3)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 1, 'number of rounds to finish', 19)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 2, 'number of players', 2)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 2, 'number of rounds to finish', 36)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 2, 'number of players', 5)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 2, 'number of rounds to finish', 15)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 3, 'number of players', 1)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 3, 'number of rounds to finish', 50)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 3, 'number of players', 5)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('rounds', 3, 'number of rounds to finish', 75)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 4, 'number of players', 4)")
+cursor.execute("INSERT INTO params (name, game_id, description, value) VALUES ('players', 4, 'number of players', 3)")
+# ScoreParams
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (0, 0)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (0, 1)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (1, 2)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (1, 3)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (2, 4)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (2, 5)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (3, 6)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (3, 7)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (4, 8)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (4, 9)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (5, 10)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (5, 11)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (6, 12)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (6, 13)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (7, 14)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (7, 15)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (8, 16)")
+cursor.execute("INSERT INTO ScoreParams (score_id, param_id) VALUES (9, 17)")
 
 conn.commit()
 conn.close()
