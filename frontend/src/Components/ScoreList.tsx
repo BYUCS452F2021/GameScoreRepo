403e6de4d93e901cf9316e09664ea5a980640614
TheMadras <braydonwhunt@gmail.com>
2021-12-08 09:43:33 -0700

    Clean up scores

--- frontend/src/Components/ScoreList.tsx
+++ frontend/src/Components/ScoreList.tsx
@@ -3,17 +3,31 @@ import AppContext from '../Contexts/AppContext'
 import './ScoreList.scss'
 
 
-type Param =  {
+type ScoreParam = {
+  value: string;
+  name: string;
+}
+
+type GameParam = {
   description: string;
   name: string;
-  value: string;
+}
+
+type Game = {
+  value: any;
+  label: any;
+  description: any;
+  availableParams: Array<GameParam>;
+  imageLink: any;
+  owner: any;
+  publisher: any;
 }
 
 type Score =  {
   game: string;
   username: string;
   timestamp: string;
-  params: Array<Param>;
+  params: Array<ScoreParam>;
 }
 
 type Props = {
@@ -24,6 +38,7 @@ type Props = {
 
 function ScoreList(props: Props) {
   const [scores, setScores] = useState<Score[]>([])
+  const [game, setGame] = useState<Game>()
   const appContext = useContext(AppContext)
 
   useEffect(() => {
@@ -34,21 +49,39 @@ function ScoreList(props: Props) {
     getScores()
   }, [appContext?.backend, props.gameName, props.userId])
 
+  useEffect(() => {
+    async function getGame() {
+      const game = props.gameName ? await appContext?.backend.getGame(props.gameName) : null
+      setGame(game)
+    }
+    getGame()
+  }, [appContext?.backend, props.gameName, props.userId])
+
   function ScoreItem(score: Score) {
     return (
       <div className='list-item' key={score.username + score.timestamp + score.game}>
         {
-          score.params.map((element: Param) => {
+          score.params.map((element: ScoreParam) => {
+            console.log(element.value)
             return (
               <div className="info-item">
-                {element.name + ': ' + element.value}
+                {element.name + ': ' + (element.value ? element.value : "")}
               </div>
             )
           })
         }
-        <div className="info-item">
-          {'Username: ' + score.username}
-        </div>
+        {
+          props.gameName &&
+          <div className="info-item">
+            {'Username: ' + score.username}
+          </div>
+        }
+        {
+          props.userId &&
+          <div className="info-item">
+            {'Game: ' + score.game}
+          </div>
+        }
       </div>
     )
   }
@@ -56,6 +89,7 @@ function ScoreList(props: Props) {
   return (
     <div className="score-page">
       <div className="list-container">
+        
         {scores.map((score: Score) => ScoreItem(score))}
       </div>
     </div>
